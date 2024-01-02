28379af346dbeb3df813e3debb8ac3370fd29692
fhasnur <fandi.hasnur10@gmail.com>
2024-01-02 22:56:17 +0800

    pagination all pages

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -7,6 +7,8 @@ import { generatePagination } from '@/app/lib/utils';
 import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
+  // NOTE: comment in this code when you get to this point in the course
+
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const currentPage = Number(searchParams.get('page')) || 1;
@@ -17,11 +19,12 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
     return `${pathname}?${params.toString()}`;
   };
 
+  const allPages = generatePagination(currentPage, totalPages);
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
 
-      {/* <div className="inline-flex">
+      <div className="inline-flex">
         <PaginationArrow
           direction="left"
           href={createPageURL(currentPage - 1)}
@@ -54,7 +57,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
           href={createPageURL(currentPage + 1)}
           isDisabled={currentPage >= totalPages}
         />
-      </div> */}
+      </div>
     </>
   );
 }
